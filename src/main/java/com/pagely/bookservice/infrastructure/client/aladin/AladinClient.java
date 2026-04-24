ff3e6f0d00422563b74805ce5478fefd48522ae2
jsh9057 <jsh9057@gmail.com>
2026-04-24 17:15:08 +0900

    feat: feignclient 해야 할 일 주석 추가

--- src/main/java/com/pagely/bookservice/infrastructure/client/aladin/AladinClient.java
+++ src/main/java/com/pagely/bookservice/infrastructure/client/aladin/AladinClient.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+// TODO: retry, CircuitBreaker 설정 추가 해야 됨
 @FeignClient(
         name = "aladin",
         url = "https://www.aladin.co.kr/ttb/api/ItemLookUp.aspx",

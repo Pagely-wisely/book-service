51c2ad2eeb5408b4b203f1096592462cc0ff3910
jsh9057 <jsh9057@gmail.com>
2026-04-24 15:54:50 +0900

    feat: Flyway 적용및 도메인 엔티티 리팩토링
    - 공통 모듈 개발 전까지 사용할 BaseEntity 정의

--- src/main/java/com/pagely/bookservice/domain/model/Book.java
+++ src/main/java/com/pagely/bookservice/domain/model/Book.java
@@ -1,53 +1,89 @@
 package com.pagely.bookservice.domain.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import java.time.LocalDateTime;
-import java.util.UUID;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
 
 @Getter
-//@Entity
-//@Table(name = "p_book)
+@Entity
+@Table(name = "p_book")
+@SQLRestriction("deleted_at IS NULL")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Book {
-    //    @Id
-//    @Column(nullable = false, length = 20)
+public class Book extends BaseEntity {
+    @Id
+    @Column(nullable = false, length = 20)
     private String id;
 
-    //    @Column(nullable = false)
+    @Column(nullable = false)
     private String title;
 
-    //    @Column(nullable = false)
-    private String author;
+    @Column(nullable = false)
+    private String authors;
 
-    //    @Column(nullable = false)
+    @Column(nullable = false)
     private String publisher;
 
-    //    @Column(name = "published_at", nullable = false)
+    @Column(name = "published_at", nullable = false)
     private LocalDateTime publishedAt;
 
-    //    @Column(name = "thumbnail_url", nullable = false, length = 500)
+    @Column(name = "thumbnail_url", nullable = false, length = 500)
     private String thumbnailUrl;
 
-    //    @Column(length = 500)
+    @Column(length = 500)
     private String description;
 
-    //    @Embedded
+    @Embedded
     private Category category;
 
-    // 감사 필드
-//    @Column(name = "created_at", nullable = false)
-    private LocalDateTime createdAt;
-    //    @Column(name = "created_by", nullable = false)
-    private UUID createdBy;
-    //    @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
-    //    @Column(name = "updated_by")
-    private UUID updatedBy;
-    //    @Column(name = "deleted_at")
-    private LocalDateTime deletedAt;
-    //    @Column(name = "deleted_by")
-    private UUID deletedBy;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id", referencedColumnName = "book_id",
+            insertable = false, updatable = false)
+    private BookStats stats;
 
+    @Builder(access = AccessLevel.PRIVATE)
+    private Book(String id, String title, String authors, String publisher,
+                 String thumbnailUrl, String description, LocalDateTime publishedAt,
+                 Long categoryId, String categoryName) {
+        this.id = id;
+        this.title = title;
+        this.authors = authors;
+        this.publisher = publisher;
+        this.thumbnailUrl = thumbnailUrl;
+        this.description = description;
+        this.publishedAt = publishedAt;
+        this.category = Category.of(categoryId, categoryName);
+    }
+
+    /*
+     * 도서 생성 팩토리 메서드
+     * 도서가 생성 되는 기준은 internal API 로 도서 정보 요청 시
+     * DB에 해당 도서에 대한 내용이 없으면, 외부 API로 도서 정보를 요청하고 저장합니다.
+     */
+    public static Book createBook(String id, String title, String authors, String publisher,
+                                  String thumbnailUrl, String description, LocalDateTime publishedAt,
+                                  Long categoryId, String categoryName) {
+
+        return Book.builder()
+                .id(id)
+                .title(title)
+                .authors(authors)
+                .publisher(publisher)
+                .thumbnailUrl(thumbnailUrl)
+                .description(description)
+                .publishedAt(publishedAt)
+                .categoryId(categoryId)
+                .categoryName(categoryName)
+                .build();
+    }
 }
--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,5 +1,52 @@
-CREATE TABLE IF NOT EXISTS test_book (
-                                         id BIGSERIAL PRIMARY KEY,
-                                         title VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- p_book
+CREATE TABLE IF NOT EXISTS p_book
+(
+    id            VARCHAR(20)  NOT NULL,
+    title         VARCHAR(255) NOT NULL,
+    authors       VARCHAR(255) NOT NULL,
+    publisher     VARCHAR(255) NOT NULL,
+    thumbnail_url VARCHAR(500) NOT NULL,
+    description   VARCHAR(500),
+    published_at  TIMESTAMP    NOT NULL,
+    category_id   BIGINT,
+    category_name VARCHAR(50),
+    created_at    TIMESTAMP    NOT NULL DEFAULT NOW(),
+    created_by    UUID         NOT NULL,
+    updated_at    TIMESTAMP,
+    updated_by    UUID,
+    deleted_at    TIMESTAMP,
+    deleted_by    UUID,
+
+    CONSTRAINT pk_book PRIMARY KEY (id)
+    );
+
+
+-- p_book_like
+CREATE TABLE IF NOT EXISTS p_book_like
+(
+    book_id    VARCHAR(20) NOT NULL,
+    user_id    UUID        NOT NULL,
+    created_at TIMESTAMP   NOT NULL DEFAULT NOW(),
+    created_by UUID        NOT NULL,
+
+    CONSTRAINT pk_book_like PRIMARY KEY (book_id, user_id)
+    );
+
+
+-- p_book_stats
+CREATE TABLE IF NOT EXISTS p_book_stats
+(
+    book_id      VARCHAR(20) NOT NULL,
+    report_count INT         NOT NULL DEFAULT 0,
+    like_count   INT         NOT NULL DEFAULT 0,
+    created_at   TIMESTAMP   NOT NULL DEFAULT NOW(),
+    created_by   UUID        NOT NULL,
+    updated_at   TIMESTAMP,
+    updated_by   UUID,
+    deleted_at    TIMESTAMP,
+    deleted_by    UUID,
+
+    CONSTRAINT pk_book_stats    PRIMARY KEY (book_id),
+    CONSTRAINT chk_report_count CHECK (report_count >= 0),
+    CONSTRAINT chk_like_count   CHECK (like_count >= 0)
     );

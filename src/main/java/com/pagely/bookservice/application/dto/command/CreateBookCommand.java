169b5b99d60d6e3f29d024b804ec1c5d63f26de3
jsh9057 <jsh9057@gmail.com>
2026-04-25 03:12:10 +0900

    refactor: CreateBookCommand validation 추가

--- src/main/java/com/pagely/bookservice/application/dto/command/CreateBookCommand.java
+++ src/main/java/com/pagely/bookservice/application/dto/command/CreateBookCommand.java
@@ -1,5 +1,7 @@
 package com.pagely.bookservice.application.dto.command;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,5 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateBookCommand {
+    @NotBlank
+    @Size(max = 20)
     private String id;
 }

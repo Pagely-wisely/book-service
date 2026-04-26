7d4e546d38768b9ff137d5dda5b2ef22f0ff35e2
jsh9057 <jsh9057@gmail.com>
2026-04-27 01:26:22 +0900

    refactor: 좋아요 주석 추가 및 코드 리팩토링
    - BookLikeId 에 Serializable이 존재하므로, BookLike에 중복 선언된 Serializable 삭제

--- src/main/java/com/pagely/bookservice/domain/model/BookLike.java
+++ src/main/java/com/pagely/bookservice/domain/model/BookLike.java
@@ -18,13 +18,25 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+/**
+ * <h1>좋아요</h1>
+ * <p>
+ * 도서 좋아요는 도서의 id를 참조하지만 FK 제약 조건이 걸려있지 않는 설계
+ * <p>
+ * 이유
+ * <ul>
+ *     <li>도서 삭제는 현재 비즈니스 로직 상 존재하지않음</li>
+ *     <li>도서가 삭제 된다면 인적 오류로, 언제든지 외부 api를 통해 도서 정보 복구 가능</li>
+ *     <li>ISBN을 key 값으로 사용하기에 어떤 경로를 통해서 도서 정보를 받아오더라도 같은 도서임이 보증됨</li>
+ * </ul>
+ */
 @Entity
 @Table(name = "p_book_like")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @IdClass(BookLike.BookLikeId.class)
 @EntityListeners(AuditingEntityListener.class)
-public class BookLike implements Serializable {
+public class BookLike {
 
     @Id
     @Column(name = "book_id", nullable = false, length = 20)

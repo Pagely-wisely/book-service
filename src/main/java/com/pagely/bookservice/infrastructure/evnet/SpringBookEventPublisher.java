3e2afab539d6748e5201bb6c3357ce7b06e663c4
jsh9057 <jsh9057@gmail.com>
2026-04-29 00:37:32 +0900

    refactor: 도서 관련 내부 이벤트를 한곳에서 발행하도록 수정

--- src/main/java/com/pagely/bookservice/infrastructure/evnet/SpringBookEventPublisher.java
+++ src/main/java/com/pagely/bookservice/infrastructure/evnet/SpringBookEventPublisher.java
@@ -2,18 +2,30 @@ package com.pagely.bookservice.infrastructure.evnet;
 
 import com.pagely.bookservice.domain.event.BookEvents;
 import com.pagely.bookservice.domain.event.payload.BookCreatedEvent;
+import com.pagely.bookservice.domain.event.payload.BookLikedEvent;
+import com.pagely.bookservice.domain.event.payload.BookUnlikedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class SpringEventPublisher implements BookEvents {
+public class SpringBookEventPublisher implements BookEvents {
 
     private final ApplicationEventPublisher applicationEventPublisher;
 
     @Override
     public void bookCreated(BookCreatedEvent event) {
+        applicationEventPublisher.publishEvent(event);
+    }
+
+    @Override
+    public void bookLiked(BookLikedEvent event) {
+        applicationEventPublisher.publishEvent(event);
+    }
+
+    @Override
+    public void bookUnliked(BookUnlikedEvent event) {
         applicationEventPublisher.publishEvent(event);
     }
 }

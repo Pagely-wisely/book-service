8ac6b4a9f41cd5f9bee7abdbc098e69d00ef1d18
jsh9057 <jsh9057@gmail.com>
2026-04-30 19:15:37 +0900

    refactor: 비동기 최소 실행 쓰레드 수 1로 수정

--- src/main/java/com/pagely/bookservice/global/config/AsyncConfig.java
+++ src/main/java/com/pagely/bookservice/global/config/AsyncConfig.java
@@ -10,7 +10,7 @@ public class AsyncConfig {
     @Bean(name = "bookGeneratorExecutor")
     public Executor bookGeneratorExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(10);
+        executor.setCorePoolSize(1);
         executor.setMaxPoolSize(10);
         executor.setQueueCapacity(50);
         executor.setThreadNamePrefix("book-generator-");
